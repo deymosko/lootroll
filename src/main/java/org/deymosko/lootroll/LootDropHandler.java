8fa35549f25ddc2575a1f0d8f01c1b743d85ad3d
deymosko <maximastoyka1@gmail.com>
2025-07-24 04:35:54 +0200

    add vote_duration and vote_radius options

--- src/main/java/org/deymosko/lootroll/LootDropHandler.java
+++ src/main/java/org/deymosko/lootroll/LootDropHandler.java
@@ -13,6 +13,7 @@ import net.minecraft.world.level.storage.loot.LootParams;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
